085d6e3b62936960310ac168a9848f21d2118b0d
kaowkaoww <270642515@yoobeestudent.ac.nz>
2025-04-24 15:17:07 +1200

    Update Homework Activity 1.2.py
    a bit revised

--- Homework Activity/Homework Activity 1-2/Homework Activity 1.2.py
+++ Homework Activity/Homework Activity 1-2/Homework Activity 1.2.py
@@ -16,22 +16,33 @@ scores = np.array([
     [90, 93, 94],
     [75, 80, 70]
 ])
+print("The test scores of 5 students across 3 subjects in a 2D :\n", scores)
+for i, student_scores in enumerate(scores, start=1):
+    print(f"Student {i}: First subject = {student_scores[0]}, Second subject = {student_scores[1]}, Third subject = {student_scores[2]}")
 
 #the average score for each student.
-student_average = np.mean(scores,axis=1)
-print("The average score for each student : ",np.round(student_average,2))
+student_average = np.mean(scores, axis=1)
+print("\nThe average score for each student:")
+for i, avg in enumerate(student_average, start=1):
+    print(f"  Student {i}: {avg:.2f}")
 
 #the average score for each subject.
 subject_average = np.mean(scores,axis=0)
-print("The average score for each subject : ",np.round(subject_average,2))
+print("\nThe average score for each subject :")
+for i, avg in enumerate(subject_average, start=1):
+    print(f"  Subject {i}: {avg:.2f}")
 
 #Identify the student (row index) with the highest total score.
 student_totalscore = np.sum(scores,axis=1)
 highest_studentscore= np.argmax(student_totalscore)
-print("Student with the highest total score (row index):",highest_studentscore)
-print("Their scores : ", scores[highest_studentscore])
+print("\nStudent with the highest total score (row index):",highest_studentscore)
+print("\nStudent",highest_studentscore+1, "got the highest total score")
+student_scores = scores[highest_studentscore]
+print(f"First subject = {student_scores[0]}, Second subject = {student_scores[1]}, Third subject = {student_scores[2]}")
 print("Their total score :", student_totalscore[highest_studentscore])
 
 #Add 5 bonus points to the third subject for all students.
 scores[:,2] += 5
-print("Score after adding bonus 5 points to the third subject: ", scores)
+print("\nScore after adding bonus 5 points to the third subject: ")
+for i, student_scores in enumerate(scores, start=1):
+    print(f"Studunt {i}: First subject = {student_scores[0]}, Second subject = {student_scores[1]}, Third subject = {student_scores[2]}")
